64178aac1024bbfb3bec724ec5d5e80db32b3b11
NTH <rmth1988@gmail.com>
2025-12-27 02:02:43 +0700

    fixbug

--- lib/ESP32WebOTA/ESP32WebOTA.cpp
+++ lib/ESP32WebOTA/ESP32WebOTA.cpp
@@ -6,16 +6,19 @@
 #include <SPIFFS.h>
 #include <esp_ota_ops.h>
 
-void ESP32WebOTA::boot() {
+void ESP32WebOTA::boot()
+{
   Preferences prefs;
   prefs.begin("ota", false);
 
   esp_ota_img_states_t state;
-  const esp_partition_t* running =
-    esp_ota_get_running_partition();
+  const esp_partition_t *running =
+      esp_ota_get_running_partition();
 
-  if (esp_ota_get_state_partition(running, &state) == ESP_OK) {
-    if (state == ESP_OTA_IMG_PENDING_VERIFY) {
+  if (esp_ota_get_state_partition(running, &state) == ESP_OK)
+  {
+    if (state == ESP_OTA_IMG_PENDING_VERIFY)
+    {
       esp_ota_mark_app_valid_cancel_rollback();
       prefs.putUInt("retry", 0);
     }
@@ -23,32 +26,43 @@ void ESP32WebOTA::boot() {
   prefs.end();
 }
 
-ESP32WebOTA::ESP32WebOTA(AsyncWebServer& server)
-  : _server(server) {}
+ESP32WebOTA::ESP32WebOTA(AsyncWebServer &server)
+    : _server(server) {}
 
-void ESP32WebOTA::begin() {
+void ESP32WebOTA::begin()
+{
+#if 1
+  // Serve static files under /ota from SPIFFS (index.html, app.js, style.css)
+  _server.serveStatic("/ota", SPIFFS, "/ota/");
 
-#if OTA_ENABLE_MANUAL
-  _server.on("/ota", HTTP_GET, [](AsyncWebServerRequest* req) {
-    if (!req->authenticate(OTA_AUTH_USER, OTA_AUTH_PASS)) {
-      return req->requestAuthentication();
-    }
-    req->send(SPIFFS, "/ota/index.html", "text/html");
+  // Avoid 500 on missing favicon requests
+  _server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest* req) {
+    req->send(204, "text/plain", "");
   });
 #endif
 
+
 #if OTA_ENABLE_MANUAL
-  _server.on("/update", HTTP_POST,
-    [](AsyncWebServerRequest* req) {
+  _server.on("/ota", HTTP_GET, [](AsyncWebServerRequest *req)
+             {
+    if (!req->authenticate(OTA_AUTH_USER, OTA_AUTH_PASS)) {
+      return req->requestAuthentication();
+    }
+    req->send(SPIFFS, "/ota/index.html", "text/html"); });
+#endif
+
+#if OTA_ENABLE_MANUAL
+  _server.on("/update", HTTP_POST, [](AsyncWebServerRequest *req)
+             {
       req->send(200, "text/plain", "OK");
-      ESP.restart();
-    },
-    [](AsyncWebServerRequest*, String, size_t idx,
-       uint8_t* data, size_t len, bool fin) {
+      ESP.restart(); }, [](AsyncWebServerRequest *, String, size_t idx, uint8_t *data, size_t len, bool fin)
+             {
 
       if (idx == 0) Update.begin(UPDATE_SIZE_UNKNOWN);
       Update.write(data, len);
-      if (fin) Update.end(true);
-    });
+      if (fin) Update.end(true); });
 #endif
+
+  _server.on("/ping", HTTP_GET, [](AsyncWebServerRequest *req)
+             { req->send(200, "text/plain", "pong"); });
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,25 @@
 
 AsyncWebServer server(80);
 
-void setup() {
-  WiFi.begin("SSID", "PASSWORD");
-  while (WiFi.status() != WL_CONNECTED) delay(200);
-
-  SPIFFS.begin(true);
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin("Papa_wifi_2.4G", "9999900000");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  Serial.println("SPIFFS mounted successfully");
 
   ESP32WebOTA::boot();
   ESP32WebOTA(server).begin();
